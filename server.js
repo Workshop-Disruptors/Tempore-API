d1925fc8790a60c9f81edcb2eea07b0de11d679e
gaelbb <32866376+gaelbb@users.noreply.github.com>
2017-10-31 13:19:14 +0100

    Connection a la base de données
    Via mongoose

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 // Récuperation des librairies de bases permettant de faire un serveur d'API
 var express = require ("express");
 var bodyParser = require("body-parser");
-var moongose = require("mongoose")
+var mongoose = require("mongoose")
 
 // Parametres du serveur
 var hostname = "localhost";
 var port = 3000;
+
+// Ces options sont recommandées par mLab pour une connexion à la base
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+
+// Adresse de la base de données
+var urlmongo = "mongodb://test-user:test-password@ds133044.mlab.com:33044/disruptors"; 
+
+// Copnnexion de l'API à la base de données
+mongoose.connect(urlmongo, options, function(err) {
+	if(err){
+console.log("Erreur lors de la connexion à la base de données")
+} else { console.log("Connexion à la base de données réussie")}
+});
 
 // Création d'un objet de type "express"
 var app = express();
@@ -15,25 +29,49 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Definiton de notre schéma de données
+var medecinSchema = mongoose.Schema({
+	nom: String,
+	ville: String,
+	tel: String,
+	description: String
+});
+
+var Medecin = mongoose.model("Medecin",medecinSchema);
+
 
 // Creation d'un Router pour faciliter le routage
 var router = express.Router();
 
-
-// Implementation pour le route /medecin
+// Implementation pour la route /medecin
 router.route("/medecin")
-// GET, POST, PUT, DELETE
 .get(function(req,res){
-	res.send("Liste tous les medecins");
+	Medecin.find(function(err, medecins){
+        if (err){
+            res.send(err); 
+        }
+        res.json(medecins); 
+        
+	})
 })
 .post(function(req,res){
-	res.json({message : "Vous avez ajouté un nouveau medecin à la liste",
-		nom : req.body.nom,
-		ville : req.body.ville,
-		method: req.method});
+	// Ajout d'un medecin suivant le model definit plus haut
+	var medecin = new Medecin();
+
+	medecin.nom = req.body.nom;
+	medecin.ville = req.body.ville;
+	medecin.tel = req.body.tel;
+	medecin.description =req.body.description;
+
+	// Enregistrement du medecin dans la base de données
+	medecin.save(function(err){
+		if(err){
+			res.send(err);
+		}
+		else {
+			res.send("Vous venez d'ajouter un médecin à Tempore")};	})
 });
 
-//Implementation pour un medecin specifique
+//Implementation pour un medecin specifique dans /medecin
 router.route("/medecin/:idMedecin")
 .get(function(req,res){
 	res.send("Vous acceder aux information du medecin "+req.params.idMedecin);
@@ -52,10 +90,9 @@ router.route("/")
 });
 
 // Initialisation du serveur avec le routage
-
 app.use(router);
 
 //Demarage du serveur
 app.listen(port, hostname, function(){
-	console.log("Mon serveur fonctionne sur http://"+hostname+":"+port+"\n")
+	console.log("Mon serveur fonctionne sur http://"+hostname+":"+port+"\n");
 });
