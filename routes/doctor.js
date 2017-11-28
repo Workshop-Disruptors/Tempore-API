f37c78b1274e521fb93b184b32c5aaea17265d37
gaelbb <32866376+gaelbb@users.noreply.github.com>
2017-11-28 16:54:30 +0100

    test7

--- routes/doctor.js
+++ routes/doctor.js
@@ -9,7 +9,7 @@ var doctor_controller = require("../controllers/doctorController");
 
 // Authentificaiton
 function requiresLogin(req, res, next) {
-  console.log("requiresLogin",req.sessionStore.sessions)
+  console.log("requiresLogin",req.sessionStore.sessions.userId)
   if (req.sessions && req.sessions.userId) {
     return next();
   } else {
