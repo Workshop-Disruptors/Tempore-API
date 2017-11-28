5d78ecdaef1c74dc23eb44a0d73f16e2b35657aa
gaelbb <32866376+gaelbb@users.noreply.github.com>
2017-11-28 12:36:23 +0100

    allow new origin from tempore.herokuapp

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(session({
 // Add headers
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100, http://tempore.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
