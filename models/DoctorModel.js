b7b9bb5f99e00f411a0ec7e3cca3bf21221c5c2e
gaelbb <32866376+gaelbb@users.noreply.github.com>
2017-12-28 22:27:17 +0100

    test2

--- models/DoctorModel.js
+++ models/DoctorModel.js
@@ -85,7 +85,7 @@ doctorSchema.pre('save', function (next) {
 });
 
 doctorSchema.pre('update', function (next) {
-	console.log("update")
+	console.log("update",doctor.password)
   var doctor = this;
   bcrypt.hash(doctor.password, 10, function (err, hash) {
     if (err) {
